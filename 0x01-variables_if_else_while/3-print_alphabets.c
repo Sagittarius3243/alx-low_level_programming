8eaa94e381cfcfd520a926dc1254ba4dcbe0abf7
Keith <keithisack@gmail.com>
2023-06-17 02:41:05 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase, followed by uppercase,
+ *              using the putchar function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowercase;
+	char uppercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	{
+		putchar(lowercase);
+	}
+
+	for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
+	{
+		putchar(uppercase);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
