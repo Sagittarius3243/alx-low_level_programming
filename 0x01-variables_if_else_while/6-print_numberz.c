85f394da52ae003c04eb776a5d55ea25e1c5f039
Keith <keithisack@gmail.com>
2023-06-17 03:03:27 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		write(1, &digit, 1);
+	}
+
+	write(1, "\n", 1);
+
+	return (0);
+}
