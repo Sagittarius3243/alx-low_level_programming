4715b73b2208c38eff8d1d031a6b930cb477e24d
Keith <keithisack@gmail.com>
2023-06-20 03:33:39 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,33 +9,26 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i < 98; i++)
 		{
-			if (i != 98)
-				_putchar(i / 10 + '0');
+			_putchar(i / 10 + '0');
 			_putchar(i % 10 + '0');
-
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
 	else
 	{
-		for (i = n; i >= 98; i--)
+		for (i = n; i > 98; i--)
 		{
-			if (i != 98)
-				_putchar(i / 10 + '0');
+			_putchar(i / 10 + '0');
 			_putchar(i % 10 + '0');
-
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
+
+	_putchar('9');
+	_putchar('8');
 	_putchar('\n');
 }
