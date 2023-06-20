a38efc7c86e7a8710db7c88ac1b2d2427dbe5212
Keith <keithisack@gmail.com>
2023-06-20 05:17:47 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,27 +1,26 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int count;
-	unsigned long fib1 = 1, fib2 = 2, fib;
+	unsigned long int a = 1, b = 2, c, count;
 
-	printf("%lu, %lu", fib1, fib2);
+	printf("%lu, %lu", a, b);
 
 	for (count = 3; count <= 98; count++)
 	{
-		fib = fib1 + fib2;
-		printf(", %lu", fib);
-		fib1 = fib2;
-		fib2 = fib;
+		c = a + b;
+		printf(", %lu", c);
+
+		a = b;
+		b = c;
 	}
 
 	printf("\n");
 
-	return (0);
+	return 0;
 }
