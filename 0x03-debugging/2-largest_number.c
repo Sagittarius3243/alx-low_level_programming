2558a5c1da4f73720f263bfcec730603fdb4f88f
Keith <keithisack@gmail.com>
2023-06-23 13:17:56 -0700

    3-print_remaining_days.c, main.h

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,17 +5,18 @@
  * @a: first integer
  * @b: second integer
  * @c: third integer
+ *
  * Return: largest number
  */
 int largest_number(int a, int b, int c)
 {
     int largest;
 
-    if (a >= b && a >= c)
+    if (a > b && a > c)
     {
         largest = a;
     }
-    else if (b >= a && b >= c)
+    else if (b > a && b > c)
     {
         largest = b;
     }
@@ -24,5 +25,5 @@ int largest_number(int a, int b, int c)
         largest = c;
     }
 
-    return largest;
+    return (largest);
 }
