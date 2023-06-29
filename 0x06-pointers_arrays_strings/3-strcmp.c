6f32bec5e5863a1f23acc9c36546b4cc778d2fbc
Keith <keithisack@gmail.com>
2023-06-29 14:30:41 -0700

    ./3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings.
+ * @s1: First string to compare.
+ * @s2: Second string to compare.
+ *
+ * Return: An integer less than, equal to, or greater than zero if s1 is found,
+ *         respectively, to be less than, to match, or to be greater than s2.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+
+	if (s1[i] == '\0' && s2[i] == '\0')
+		return (0);
+	else if (s1[i] == '\0')
+		return (-s2[i]);
+	else
+		return (s1[i]);
+}
