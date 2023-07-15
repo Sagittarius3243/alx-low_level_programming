32c899ae5649f3571f34d549c2ff26b22e1b4737
Keith <keithisack@gmail.com>
2023-07-15 04:17:13 -0700

    4-free_grid.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 
 /**
- * create_array - Creates an array of chars and initializes it with a specific char
- * @size: The size of the array
- * @c: The char to initialize the array with
+ * create_array - Creates an array of chars and initializes it with a specific char.
+ * @size: The size of the array.
+ * @c: The character to initialize the array with.
  *
- * Return: A pointer to the created array, or NULL if it fails
+ * Return: If the size is 0 or if memory allocation fails - NULL.
+ *         Otherwise - a pointer to the array.
  */
 char *create_array(unsigned int size, char c)
 {
@@ -17,7 +18,6 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 
 	array = malloc(sizeof(char) * size);
-
 	if (array == NULL)
 		return (NULL);
 
