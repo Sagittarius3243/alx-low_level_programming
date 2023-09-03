dc10b6ed2b6a09e5601a4aa94a42a2f3da2455fa
Keith <keithisack@gmail.com>
2023-09-02 23:34:45 -0700

    ./0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,16 +3,17 @@
 /**
  * _puts_recursion - Prints a string followed by a new line recursively.
  * @s: The string to be printed.
+ *
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		_putchar(*s);            /* Print the current character */
-		_puts_recursion(s + 1);  /* Recursively call the function with the next character */
+		_putchar('\n'); /* Print a new line when the end of the string is reached */
+		return;
 	}
-	else
-	{
-		_putchar('\n');  /* Base case: end of the string, print a new line */
-	}
+
+	_putchar(*s);              /* Print the current character */
+	_puts_recursion(s + 1);    /* Recursively call with the next character */
 }
