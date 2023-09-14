0e1594aa8098d47ad46ccf23f310da8ec4f9445c
Keith <keithisack@gmail.com>
2023-09-14 09:18:59 -0700

    ./a

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,24 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+
+/**
+ * struct format - Struct format
+ *
+ * @format: The format specifier
+ * @func: The associated function
+ */
+typedef struct format
+{
+    char *format;
+    void (*func)(va_list);
+} format_t;
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H */
